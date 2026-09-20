cd7204999c6ca3db86751900b1bd49069880af52
jkulmus <kul18001@byui.edu>
2026-09-19 21:32:22 -0700

    finished asp.net core crud api

--- unit01/ContosoPizza/Controllers/PizzaController.cs
+++ unit01/ContosoPizza/Controllers/PizzaController.cs
@@ -27,9 +27,44 @@ public class PizzaController : ControllerBase
         return pizza;
     }
 
-    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
 
-    // PUT action
+        return CreatedAtAction(
+            nameof(Get),
+            new { id = pizza.Id },
+            pizza
+        );
+    }
 
-    // DELETE action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
